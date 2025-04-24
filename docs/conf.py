5fe89a39d607169e77b1481b25af2c246b18d7f8
Alper <alper.yurtseven@helmholtz-hips.de>
2025-04-24 14:33:31 +0200

    fixed version issue

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,8 @@ import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath("./.."))
 sys.path.insert(0, os.path.abspath("./."))
 
+from sr_amr.version import __version__
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
