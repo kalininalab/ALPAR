c7f3a118dfd912ce7fbada019d427515bbc0cfa7
Cambouu <josemcv1011@hotmail.com>
2026-02-06 16:59:21 +0100

    changing from flood_fill to filtered fill

--- snakefiles/scripts/binary_gpa_cdhit.py
+++ snakefiles/scripts/binary_gpa_cdhit.py
@@ -1,5 +1,6 @@
 
 from contextlib import suppress
+from typing import Generator
 
 import polars as pl
 from pydantic import BaseModel, Field, FilePath, NewPath
@@ -11,90 +12,89 @@ class SnakemakeHandler(BaseModel):
     """Validator for snakemake io."""
 
     clstr_file: FilePath = Field(
-        description='Path to the cd-hit .clstr cluster file.'
+        description="""Path to the cd-hit .clstr cluster file.
+
+        The .clstr file looks like this:
+        >Cluster 0
+        0	1804aa, >676637238abec74c8061925ed17638bd3d6f79b7_EHGLCAJA_00557_hypothetical_protein... at 97.78%
+        1	6925aa, >15ecf424907d408f7d2b86d88716d49c3ba6f70e_IFBKKINJ_01001_hypothetical_protein... *
+        2	6925aa, >1dd8eefe37b479270ec8213260cf3f0abdf90125_MHDMJHDI_04197_hypothetical_protein... at 96.68%
+        3	6925aa, >c3c3454a0958fbfc725980cfec6bced497b1f7c0_BDFOFLOO_00761_hypothetical_protein... at 97.13%
+        >Cluster 1
+        0	5423aa, >676637238abec74c8061925ed17638bd3d6f79b7_EHGLCAJA_00556_hypothetical_protein... *
+        """
     )
     output_file: NewPath = Field(
         description='Path to where the output file will be saved.'
     )
 
-
     def binary_gpa_cdhit(self) -> None:
-        """Create Protein Presence/Absence information as a binary table.
-        
-        The .clstr file looks like this:
-        >Cluster 0
-        0	6925aa, >AG2T4UON_BDFOFLOO_00761_hypothetical_protein... *
-        1	6925aa, >BR0MOR20_MHDMJHDI_04197_hypothetical_protein... at 96.81%
-        2	1804aa, >ITJA290B_EHGLCAJA_00557_hypothetical_protein... at 97.95%
-        3	6925aa, >UAS7U9ST_IFBKKINJ_01001_hypothetical_protein... at 97.13%
-        >Cluster 1
-        0	5423aa, >ITJA290B_EHGLCAJA_00556_hypothetical_protein... *
-        """
+        """Create Protein Presence/Absence information as a binary table."""
 
-        clstr_id = None
-        all_proteins = list[tuple[str, str]]()
-        with open(self.clstr_file, 'r', encoding='utf-8') as f:
-
-            for line in f:
-                line = line.strip()
-
-                if line.startswith('>'):
-                    clstr_id = line
-                    continue
-
-                assert clstr_id is not None, \
-                    'The first line of the .clstr file should start with the cluster ID.'
-
-                all_proteins.append((clstr_id, line))
-
-        # Dataframe of shape (n_proteins, 2)
-        # where the first column is the cluster ID and the second column is the protein raw data.
-        df_clusters_cdhit = pl.DataFrame(
-            all_proteins,
+        df_clusters_cdhit = pl.LazyFrame(
+            self.zip_header_and_content(self.clstr_file),
             schema=(('cluster', pl.String), ('protein_raw', pl.String)),
             orient='row',
         )
 
         # Extract the protein name and similarity from the raw data.
         protein_raw_regex = r'^\d+\t+\d+aa, >(?P<protein>\w+)\.{3} (?:\*|at (?P<similarity>\d+\.\d+)%)$'
-        df_regex = df_clusters_cdhit.with_columns(
-            captures=(
-                pl.col('protein_raw')
-                .str.extract_groups(protein_raw_regex)
-            ),
-        ).unnest('captures')
-
-        # The protein section before the underscore is the strain
-        df_with_strain = df_regex.with_columns(
-            pl.col('protein').cast(pl.String),
-            pl.col('similarity').cast(pl.Float64),
-            value=pl.lit(1, pl.UInt8), # This will help us with the binary pivot table.
-            Strain=(
-                pl.col('protein')
-                .str.extract(r'^(?P<Strain>[a-zA-Z0-9]+)', 1)
+        df_regex = (
+            df_clusters_cdhit.with_columns(
+                captures=(
+                    pl.col('protein_raw').str.extract_groups(protein_raw_regex)
+                ),
+            )
+            .unnest('captures')
+            .with_columns(
+                pl.col('protein').cast(pl.String),
+                pl.col('similarity').cast(pl.Float64),
+                value=pl.lit(1, pl.UInt8), # This will help us with the binary pivot table.
+                Strain=pl.col('protein').str.extract(r'^(?P<Strain>[a-zA-Z0-9]+)', 1)
             )
         )
 
         # Whenever the similarity is null, that is the cluster reference.
         # We will propagate the protein name to all rows within the same cluster.
-        df_fill = df_with_strain.with_columns(
-            pl.when(pl.col('similarity').is_null())
-            .then(pl.col('protein'))
-            .forward_fill()
-            .over(pl.col('cluster'))
-            .alias('protein_fill')
+        df_fill = df_regex.with_columns(
+            protein_ref = (
+                pl.col('protein')
+                .filter(pl.col('similarity').is_null())
+                .first()
+                .over(pl.col('cluster'))
+            )
         )
 
         # First column is the cluster reference protein, columns are the strains.
-        df_pivot = df_fill.pivot(
-            on='protein_fill',
-            index='Strain',
-            values='value',
-            aggregate_function='first',
-        ).fill_null(0)
+        # We have to resolve the LazyFrame because we do not know in advance the protein names.
+        df_pivot = (
+            df_fill
+            .collect()
+            .pivot(
+                on='protein_fill',
+                index='Strain',
+                values='value',
+                aggregate_function='first',
+            )
+            .fill_null(0)
+        )
 
         with open(self.output_file, 'w', encoding='utf-8') as f:
             df_pivot.write_csv(f, include_header=True, separator='\t')
+
+    @staticmethod
+    def zip_header_and_content(file: FilePath) -> Generator[tuple[str, str]]:
+        """Given a fasta file, yield tuples of (header, content) for each sequence."""
+        header = ''
+        with open(file, 'r', encoding='utf-8') as f:
+            for line in iter(f):
+                line = line.strip()
+
+                if line.startswith('>'):
+                    header = line
+                    continue
+
+                yield header, line
 
 
 if __name__ == '__main__':
